363100b3e02316e507dd6eee020d7b63a23d153f
BigggOunce <153123150+BigggOunce@users.noreply.github.com>
2023-12-06 19:26:02 +0000

    README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# yeahyeah
-yeahyeah it be bigounce doing thangs on the computer, doing the computer speak, for sure haha yeah..
---
+++ README.py
@@ -0,0 +1,19 @@
+# yeahyeah
+print("yeahyeah it be bigounce doing thangs on the computer, doing the computer speak, for sure haha yeah..")
+
+
+#look at bigggounce do his thang, wathc me go
+
+for x in range(1,51):
+  print("\tIt be big ounce dawg")
+
+#watch me  go
+
+bigList = ['watch','me','go.','big','ounce','does','the','bounce']
+i=0
+bigdex=0
+while i < len(bigList):
+  print(bigList[bigdex])
+  i += 1
+  bigdex += 1
+  
