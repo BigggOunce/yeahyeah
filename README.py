903a90ca11c5a61703d19b9515cad27320f61d30
BigggOunce <153123150+BigggOunce@users.noreply.github.com>
2024-07-05 04:00:31 +0100

    README.py

--- README.py
+++ README.py
@@ -4,16 +4,6 @@ print("yeahyeah it be bigounce doing thangs on the computer, doing the computer 
 
 #look at bigggounce do his thang, wathc me go
 
-for x in range(1,51):
+for i in range(5):
   print("\tIt be big ounce dawg")
-
-#watch me  go
-
-bigList = ['watch','me','go.','big','ounce','does','the','bounce']
-i=0
-bigdex=0
-while i < len(bigList):
-  print(bigList[bigdex])
-  i += 1
-  bigdex += 1
   
